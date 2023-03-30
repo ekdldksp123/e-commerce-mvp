dd151e5e27be08796663daefe73c3b68545f0a7d
vinchae <ekdldksp123@naver.com>
2023-03-30 23:09:26 +0900

    :lipstick: update product list

---
+++ src/components/molecules/product.tsx
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+import { FC } from 'react';
+import { BsCartPlusFill } from 'react-icons/bs';
+import { ProductData } from '~/types/product';
+
+const Product: FC<ProductData> = ({ id, title, price, description, image }) => {
+  return (
+    <div className='col'>
+      <div className='card shadow-sm'>
+        <Link to='/products/1' replace>
+          <img className='card-img-top bg-dark cover' height='200' alt='' src={image} />
+        </Link>
+        <div className='card-body'>
+          <h5 className='card-title text-center text-dark text-truncate'>{title}</h5>
+          <p className='card-text text-center text-muted mb-0'>{price}</p>
+          <div className='d-grid d-block'>
+            <button className='btn btn-outline-dark mt-3'>
+              <BsCartPlusFill size={25} /> Add to cart
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
--- src/components/views/product-list.tsx
+++ src/components/views/product-list.tsx
@@ -1,9 +1,12 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { useQuery } from 'react-query';
 import { getAllProducts } from '~/libs/api.module';
 import { ProductData } from '~/types/product';
+import ScrollToTopOnMount from '../atoms/scroll-to-top';
+import Product from '../molecules/product';
 
 const ProductList: FC = () => {
+  const [viewType, setViewType] = useState({ grid: true });
   const { isLoading, error, data } = useQuery('products', getAllProducts);
 
   if (isLoading) {
@@ -15,11 +18,19 @@ const ProductList: FC = () => {
   }
 
   return (
-    <ul>
-      {data.map((product: ProductData) => (
-        <li key={product.id}>{product.title}</li>
-      ))}
-    </ul>
+    <div className='container mt-5 py-4 px-xl-5'>
+      <ScrollToTopOnMount />
+      <div
+        className={
+          'row row-cols-1 row-cols-md-2 row-cols-lg-2 g-3 mb-4 flex-shrink-0 ' +
+          (viewType.grid ? 'row-cols-xl-3' : 'row-cols-xl-2')
+        }
+      >
+        {data.map((product: ProductData) => (
+          <Product key={product.id} {...product} />
+        ))}
+      </div>
+    </div>
   );
 };
 
