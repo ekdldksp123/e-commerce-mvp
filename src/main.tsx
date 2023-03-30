876843f5c780bdb26b37f4a6ce0aea5a121d2e05
vinchae <ekdldksp123@naver.com>
2023-03-30 12:31:29 +0900

    :heavy_plus_sign: add a react-query dependency :hammer: add QueryClientProvider on main

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import App from './App';
 
 // styles
 import './index.css';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
